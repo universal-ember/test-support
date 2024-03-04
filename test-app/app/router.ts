440ef264e8e5771e3757e066920ab743e41e4909
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-03 23:22:41 -0500

    Gotta use proper links

--- test-app/app/router.ts
+++ test-app/app/router.ts
@@ -1,6 +1,9 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'test-app/config/environment';
 
+import { properLinks } from 'ember-primitives/proper-links';
+
+@properLinks
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
@@ -8,4 +11,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   // Add route declarations here
+  this.route('*any');
 });
