3385d7125d079f224d2c29bb7fff2d22b7d917ea
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-12 21:51:33 -0400

    type tests

--- test-app/tests/acceptance/get-service-test.ts
+++ test-app/tests/acceptance/get-service-test.ts
@@ -1,13 +1,16 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
+import { expectTypeOf } from 'expect-type';
 
 import { getService } from '@universal-ember/test-support';
+import RouterService from '@ember/routing/router-service';
 
 module('getService', function (hooks) {
   setupApplicationTest(hooks);
 
   test('gets the service', async function (assert) {
     const router = getService('router');
+    expectTypeOf(router).toEqualTypeOf<RouterService>();
 
     assert.ok(router);
   });
