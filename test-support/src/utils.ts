c9913fc9b21f4556c885770edbcc43d5781d85ab
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-28 15:52:57 -0400

    Add readme

---
+++ test-support/src/utils.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
+
+/**
+ * A no-op function that literally does nothing.
+ */
+export function noop(...args: any[]): any {}
